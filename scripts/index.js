ede7b30ca76d07664eccc2eca76f0a9950acae6a
OksanaKam <moremore1@yandex.ru>
2023-01-19 22:10:15 +0200

    ПР4 итерация2

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
 let button = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let closePopup = document.querySelector(".popup__close");
-let submitPopup = document.querySelector(".popup__submit-button");
-let like = document.querySelector(".element__like");
-let container = document.querySelector(".popup__container")
+let container = document.querySelector(".popup__container");
 let nameInput = container.querySelector(".popup__input_name_title");
 let jobInput = container.querySelector(".popup__input_name_yourself");
 let profileTitle = document.querySelector(".profile__title");
@@ -21,7 +19,6 @@ function closeForm() {
     popup.classList.remove("popup_opened");
 }
 
-container.addEventListener('submit', handleFormSubmit);
 closePopup.addEventListener("click", closeForm);
 
 function handleFormSubmit (evt) {
@@ -31,9 +28,4 @@ function handleFormSubmit (evt) {
     closeForm();
 }
 
-function clickLike(event) {
-    event.preventDefault();
-    like.classList.add("element__like_active");
-}
-
-like.addEventListener("click", clickLike);
+container.addEventListener('submit', handleFormSubmit);
